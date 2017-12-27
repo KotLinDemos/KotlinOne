017ee5aa3b5d56d7b5c1a9f8abf07ccd0af6dfa5
史少斌 <1135021043@qq.com>
2017-12-27 19:33:57 +0800

    11

--- KotLinView/app/src/main/java/com/bwie/fragments/HotFragment.kt
+++ KotLinView/app/src/main/java/com/bwie/fragments/HotFragment.kt
@@ -1,19 +1,18 @@
 package com.bwie.fragments
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.bwie.kotlinview.R
 
 /**
  * Created by ${单巨廷} on 2017/12/27.
  */
-class HotFragment : Fragment() {
+class HotFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.hot_fragment, container, false)
-        return view
+
+    override fun getLayoutId(): Int {
+        return R.layout.hot_fragment
+    }
+
+    override fun initview() {
+        var weekFragment : SumFragment
     }
 }
