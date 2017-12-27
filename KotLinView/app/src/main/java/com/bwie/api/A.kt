904796bbd9308c34a3f4de3bdf3c1fa8ed9fb5aa
李亚亚 <1564088177@qq.com>
2017-12-27 15:22:48 +0800

    李亚亚
    Apiservice

--- KotLinView/app/src/main/java/com/bwie/api/A.kt
+++ KotLinView/app/src/main/java/com/bwie/api/A.kt
@@ -3,5 +3,9 @@ package com.bwie.api
 /**
  * Created by ${单巨廷} on 2017/12/27.
  */
-class A {
+class A{
+    //http://baobab.kaiyanapp.com/api/v2/feed?num=2&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
+    companion object {
+        var homefragment="http://baobab.kaiyanapp.com/api/v2/"
+    }
 }
