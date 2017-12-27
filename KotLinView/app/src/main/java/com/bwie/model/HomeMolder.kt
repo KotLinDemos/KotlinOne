9690ebfec8591fa78bed8157a6c890afc32d7425
李亚亚 <1564088177@qq.com>
2017-12-27 16:59:06 +0800

    liyaya
    home

---
+++ KotLinView/app/src/main/java/com/bwie/model/HomeMolder.kt
@@ -0,0 +1,24 @@
+package com.bwie.model
+
+import com.bwie.api.A
+import com.bwie.api.ApiService
+import com.bwie.bean.Homebean
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import io.reactivex.Flowable
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by dell on 2017/12/27.
+ */
+class HomeMolder{
+    fun getDataMolder():Flowable<List<Homebean>>{
+        var retrofit=Retrofit.Builder().baseUrl(A.homefragment)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        val apiService = retrofit.create(ApiService::class.java)
+        val flowable = apiService.getHome("2", "26868b32e808498db32fd51fb422d00175e179df", "83")
+        return flowable
+    }
+}
