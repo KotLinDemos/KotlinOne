5d4273ec0e14722f74004cdc7dd27d1366129f39
史少斌 <1135021043@qq.com>
2017-12-27 15:36:38 +0800

    123

---
+++ KotLinView/app/src/main/java/com/bwie/view/BaseActivity.kt
@@ -0,0 +1,29 @@
+package com.bwie.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity<T> : AppCompatActivity() {
+
+    var presenter:T? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId())
+        initView()
+        getPresenter()
+        initData()
+    }
+
+
+
+
+    //获取布局Id
+    abstract fun layoutId(): Int
+    //初始化数据
+    abstract fun initView()
+    //关联P层
+    internal abstract fun getPresenter():T
+    //展示数据
+    abstract fun initData()
+
+}
