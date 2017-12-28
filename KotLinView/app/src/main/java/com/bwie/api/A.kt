f4d448b73169b36ff7ab7c36dd79a2c1101494aa
史少斌 <1135021043@qq.com>
2017-12-28 10:56:19 +0800

    789

--- KotLinView/app/src/main/java/com/bwie/api/A.kt
+++ KotLinView/app/src/main/java/com/bwie/api/A.kt
@@ -7,5 +7,7 @@ class A{
     //http://baobab.kaiyanapp.com/api/v2/feed?num=2&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
     companion object {
         var homefragment="http://baobab.kaiyanapp.com/api/v2/"
+
+        var TEST = "http://baobab.wandoujia.com/api/"
     }
 }
--- KotLinView/app/src/main/java/com/bwie/api/ApiService.kt
+++ KotLinView/app/src/main/java/com/bwie/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.bwie.api
 
 import com.bwie.bean.Homebean
+import com.bwie.bean.HotBean
 import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,4 +13,8 @@ interface ApiService{
     //http://baobab.kaiyanapp.com/api/v2/feed?num=2&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83
     @GET("feed")
     fun getHome(@Query("num") num:String,@Query("udid") udid:String,@Query("vc") vc:String):Flowable<List<Homebean>>
+
+    @GET("v3/ranklist")
+    fun getHotData(@Query("num")num:Int,@Query("strategy")strategy:String,@Query("udid")udid:String,
+                   @Query("vc")vc:Int) : Flowable<HotBean>
 }
