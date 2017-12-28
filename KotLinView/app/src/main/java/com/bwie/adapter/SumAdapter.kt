209c9be579787532094de9c0a2d47c7c37bea8dd
史少斌 <1135021043@qq.com>
2017-12-28 11:15:01 +0800

    456

--- KotLinView/app/src/main/java/com/bwie/adapter/SumAdapter.kt
+++ KotLinView/app/src/main/java/com/bwie/adapter/SumAdapter.kt
@@ -1,15 +1,15 @@
 package com.bwie.adapter
 
 import android.content.Context
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import com.bwie.bean.HotBean
 import com.bwie.kotlinview.R
-import com.squareup.picasso.Picasso
+import com.facebook.drawee.view.SimpleDraweeView
 
 /**
  * Created by 朝朝暮暮 on 2017/12/28.
@@ -24,7 +24,7 @@ class SumAdapter(context:Context,list:List<HotBean.ItemListBean>) : RecyclerView
 
         viewholder.tv_connect.setText(mlist.get(position).data!!.title)
         viewholder.tv_blow.setText(mlist.get(position).data!!.category+"/")
-        Picasso.with(mcontext).load(mlist.get(position).data!!.cover!!.detail).into(viewholder.iv_bg)
+        viewholder.iv_bg.setImageURI(Uri.parse(mlist.get(position).data!!.cover!!.detail))
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
@@ -36,7 +36,7 @@ class SumAdapter(context:Context,list:List<HotBean.ItemListBean>) : RecyclerView
     }
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        var iv_bg : ImageView = itemView.findViewById(R.id.iv_bg)
+        var iv_bg : SimpleDraweeView = itemView.findViewById(R.id.iv_bg)
         var tv_connect : TextView = itemView.findViewById(R.id.tv_connect)
         var tv_blow : TextView = itemView.findViewById(R.id.tv_blow)
     }
